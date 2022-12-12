f8e2890dbda7ef0d43f3c502aa9d0ee489919ed2
heoeuntaek <gjdmsxor@naver.com>
2022-12-12 20:02:35 +0900

    d

---
+++ app/src/main/java/com/example/a201501496het/Spinner.kt
@@ -0,0 +1,66 @@
+package com.example.a201501496het
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class Spinner : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spinner)
+
+        val fruit = resources.getStringArray(R.array.fruit)
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item, fruit
+        )
+        val spinner: Spinner = findViewById(R.id.spinner)
+        spinner.adapter = adapter
+
+        adapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        spinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?, position: Int, id: Long
+                ) {
+                    var planet = parent?.getItemAtPosition(position).toString()
+
+                    Toast.makeText(
+                        parent?.context,
+                        "선택된 행성은 $planet",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
+    }
+}
+
